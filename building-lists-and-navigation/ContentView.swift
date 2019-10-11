74a042f274f7b04c27f861f38a1492e931c6892c
Yota Suzuki <apple.youta1214@gmail.com>
2019-10-12 08:56:19 +0900

    Initial Commit

---
+++ building-lists-and-navigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  building-lists-and-navigation
+//
+//  Created by suzuki_y on 2019/10/12.
+//  Copyright © 2019 Yota Suzuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ building-lists-and-navigationTests/building_lists_and_navigationTests.swift
@@ -0,0 +1,34 @@
+//
+//  building_lists_and_navigationTests.swift
+//  building-lists-and-navigationTests
+//
+//  Created by suzuki_y on 2019/10/12.
+//  Copyright © 2019 Yota Suzuki. All rights reserved.
+//
+
+import XCTest
+@testable import building_lists_and_navigation
+
+class building_lists_and_navigationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
