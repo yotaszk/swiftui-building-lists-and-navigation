faf4bb1364974c7429fcebe3a77a89d760377c59
Yota Suzuki <apple.youta1214@gmail.com>
2019-10-12 15:22:20 +0900

    first commit

---
+++ building-lists-and-navigation/pages/LandmarkDetail.swift
@@ -0,0 +1,41 @@
+//
+//  LandmarkDetail.swift
+//  building-lists-and-navigation
+//
+//  Created by suzuki_y on 2019/10/12.
+//  Copyright © 2019 Yota Suzuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack(alignment: .top) {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
---
+++ building-lists-and-navigation/pages/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  building-lists-and-navigation
+//
+//  Created by suzuki_y on 2019/10/12.
+//  Copyright © 2019 Yota Suzuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(frame(width: 50, height: 50, alignment: .center))
+            Text(landmark.name)
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
